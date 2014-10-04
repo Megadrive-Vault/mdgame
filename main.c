19e583d85efb9fa548846f7ee47715c5a93066da
Michael Moffitt <mikejmoffitt@gmail.com>
2014-10-04 16:26:37 -0400

    Wrote a little PSG test function

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <genesis.h>
 
+
+void psg_tone(u8 chan, u8 vol, u16 pitch)
+{
+	u8 *psg = (u8 *)0xC00011;
+	*psg = 0x90 | ((chan & 0x03) << 5) | (vol & 0x0F);
+	asm("nop");
+	*psg = 0x80 | ((chan & 0x03) << 5) | (pitch & 0x0F);
+	asm("nop");
+	*psg = pitch >> 4 & 0x3F;
+}
+
 int main(void)
 {
         VDP_drawText("Manual controller reads...", 1, 1);
@@ -13,22 +24,33 @@ int main(void)
 		VDP_drawText("Start",5,10);
 
 		u8 *ctrlr;
-		ctrlr = 0x00A10003;
+		ctrlr = (u8 *)0x00A10003;
 		
-		int i = 0;
 		int k = 0;
+		u16 pitch = 0;
+		
+		psg_tone(0,8,1400);
 		
         while(1)
         {
+		
+			//psg_tone(1,7,pitch + 128);
+			pitch = pitch + 1;
+			psg_tone(3,0,0xFFFF - pitch / 2);
+			psg_tone(0,(pitch<<1)&0xF,0xFFFF - pitch);
+			psg_tone(1,(pitch>>1)&0x0F,pitch + 128);
+			psg_tone(2,(pitch>>2)&0x0F,pitch + 384);
 			// Set bit 7 to read 0SA00DU
 			
 			
 			// Clr bit 7 to read 1CBRLDU
 			*ctrlr = *ctrlr | (0x40);
+			asm("nop");
 			
 			// Paul-quality test code
 			
-			
+		
+			/*
 			for (k = 0; k < 8; k++)
 			{
 				if ((*ctrlr >> k) & 0x01)
@@ -40,6 +62,7 @@ int main(void)
 					VDP_drawText("0",16,3+k);
 				}
 			}
+			*/
 			
 			if (*ctrlr & 0x01) // U
 			{
@@ -94,7 +117,11 @@ int main(void)
 			
 			
 			*ctrlr = *ctrlr & ~(0x40);
-			
+			for (k = 0; k < 8; k++)
+			{
+			asm("nop");
+			}
+			/*
 			for (k = 0; k < 8; k++)
 			{
 				if ((*ctrlr >> k) & 0x01)
@@ -106,7 +133,7 @@ int main(void)
 					VDP_drawText("0",18,3+k);
 				}
 			}
-			
+			*/
 			if (*ctrlr & 0x10)
 			{
 				VDP_drawText("A",3,7);
