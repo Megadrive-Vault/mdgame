47e1f7d83b8c906bd595f65ff51a5a4fff5a98c4
Michael Moffitt <mikejmoffitt@gmail.com>
2014-10-04 13:29:48 -0400

    Got a basic windows build batch together for SGDK

---
+++ buildrun.bat
@@ -0,0 +1,2 @@
+%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen
+Fusion out/rom.bin
---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <genesis.h>
+
+int main()
+{
+        VDP_drawText("Hello Genny World!", 10, 13);
+
+        while(1)
+        {
+                //read input
+                //move sprite
+                //update score
+                //draw current screen (logo, start screen, settings, game, gameover, credits...)
+        }
+        return (0);
+}
