710092fd63b56c7231a528a61c698f31845345eb
Michael Moffitt <mikejmoffitt@gmail.com>
2014-10-12 03:09:02 -0400

    Fixed build to work better with music, included WIP birds

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-util\xm2esf-master\xm2esf res\xm\conv.xif res\music\bgm.esf
+cd util\xm2esf-master\
+xm2esf ..\..\res\xm\conv.xif ..\..\res\music\bgm.esf
+cd ..\..
 %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen
